f08375fde85cb306eeb17f3245a5a9d130f32283
sb255 <samarthbais1@gmail.com>
2019-11-10 11:26:59 +0530

    File added

--- Recursion/BasicRecursion.java
+++ Recursion/BasicRecursion.java
@@ -21,8 +21,21 @@ public class BasicRecursion{
 }
 
 /*
-
 Recursion stack of the function printValues will be:
 f(f(f(f(f(f(f(f(f(f(f(0)))))))))))
+
+Output:
+
+END OF RECURSION
+Function printValues executed completly with n = 1
+Function printValues executed completly with n = 2
+Function printValues executed completly with n = 3
+Function printValues executed completly with n = 4
+Function printValues executed completly with n = 5
+Function printValues executed completly with n = 6
+Function printValues executed completly with n = 7
+Function printValues executed completly with n = 8
+Function printValues executed completly with n = 9
+Function printValues executed completly with n = 10
 
 */
